a5b258903f50e66925ef12d9a883c6322746c4a1
vdorovatas <vdorovatas@hotmail.gr>
2020-05-09 13:53:43 +0000

    BSTree done

--- BTree/btree.cpp
+++ BTree/btree.cpp
@@ -16,36 +16,75 @@ node(const string &s, node *l, node *r, node *par, int d) : word(s),left(l), rig
 node *root;
 
 node* preorder (node *t, const string &s) {
-  if (s == t->word) { return t;}
-  preorder(t->left, s);
-  preorder(t->right, s);
-  return NULL;
+  if(t==NULL || t->word == s) { return t; }
+  if(s < t->word) preorder(t->right, s);
+  else if(s > t->word) preorder(t->left, s);
+  }
+
+
+node * right(node *p) const{
+node *t;
+t=p;
+while(t->right!=NULL) { t = t->right; }
+return t;
 }
 
+void print(ostream &out, node *t) const {
+node *p;
+p=t;
+out << p->word << " " << p->freq << endl;
+if(p->left != NULL) print(out, right(p->left));
+if(p->parent == NULL) { return; }
+ if((p->parent)->right == p) {
+ print(out,p->parent);
+}
+if((p->parent)->left == p) {
+ p=(p->parent)->parent;
+}
+}
 
+void del (node *p) {
+if(p==NULL) { return; }
+ del(p->right);
+ del(p->left);
+ delete p;
+}
+
+void fix_depth (node *p, int d) {
+if(p!=NULL) {
+p->dep = d;
+fix_depth(p->right, d+1);
+fix_depth(p->left, d+1);
+}
+}
 public:
   lexicon() {
     root=NULL;
   }
 
-  ~lexicon();
+  ~lexicon() {
+  del(root);
+  }
 
-  void insert(const string &s) {
+  void insert(const string &s, int f=1) {
     node *p;
     p=root;
     node *n=new node(s,NULL,NULL,NULL,1);
-     if(root==NULL) { root=n; } 
+    n->freq = f;
+     if(root==NULL) { root=n; }
+     else {
     while(1==1) {
       if(s == p->word) { ++p->freq; delete n; break; }
       if(s < p->word) {
-        if(p->left==NULL) { p->left=n; n->dep=(p->dep+1); n->parent = p; break; }
-        p=p->left;
-      }
-      if(s > p->word) {
-        if(p->right==NULL) { p->right=n; n->dep=(p->dep)+1; n->parent = p; break; }
+        if(p->right==NULL) { p->right = n; n->dep = (p->dep)+1; n->parent = p; break; }
         p=p->right;
       }
+      if(s > p->word) {
+        if(p->left==NULL) { p->left = n; n->dep = (p->dep)+1; n->parent = p; break; }
+        p=p->left;
+      }
     }
+     }
   }
 
   int lookup (const string &s) {
@@ -53,7 +92,8 @@ public:
     node *t;
     p=root;
     t=preorder(p, s);
-    return t->freq;
+    if(t==NULL) { return 0; }
+    else return t->freq;
   }
 
   int depth (const string &s) {
@@ -61,6 +101,7 @@ public:
     p=root;
     node *t;
     t=preorder(p, s);
+    if(t==NULL) return 0;
     return t->dep;
   }
 
@@ -72,55 +113,75 @@ public:
     if(t!=NULL) {
       n = preorder(p, s2);
       if(n!=NULL) { n->freq = n->freq + t->freq; }
-      else { insert(s2); }
+
+      else { insert(s2, t->freq);  }
+
+      if(t->right == NULL && t->left == NULL) {
+           if(t->parent != NULL) {
+                if((t->parent)->word > t->word) { (t->parent)->right = NULL; }
+                if((t->parent)->word < t->word) { (t->parent)->left = NULL; }
+              }  delete t; }
+
+      else {
+
+      if(t->right == NULL) { t->right = t->left; t->left = NULL; }
       q = t->parent;
-      if(q->word > t->word) { q->right = t->right;}
-      else if(q->word < t->word) { q->left = t->right; }
-      n = t->right;
-      t = t->left; 
-      while((n->left)!=NULL && (n->right)!=NULL) {
-        if((n->right)->word < t->word  && t->word < (n->left)->word ) {
-          p = n->right; //ksemparko
-          n->right = t;
-          t = p;
-          n = n->right;
+
+      if(q!=NULL) {
+        if(q->word > t->word) { q->right = t->right; (t->right)->parent = q; } //--(t->right)->dep; }
+        else if(q->word < t->word) { q->left = t->right; (t->right)->parent = q; } //--(t->right)->dep; }
         }
-        else if( t->word > (n->left)->word) {
-          p = n->left;
-          n->left = t;
-          t = p;
-          n = n->left;
-        }
-        else if( t->word < (n->right)->word) {
-          n = n->right;
-        }
+      else if(q==NULL) {(t->right)->parent = NULL; root = t->right; }
+
+      q = t->left;                       //ksemparko
+      if(q!=NULL) { q->parent = NULL; }
+      n = t->right;                     //new parent
+      delete t;
+
+      if(q!=NULL) {
+      while(n->left!=NULL) { n = n->left; }
+      n->left = q;
+      q->parent = n;
       }
-      if(n->word > t->word) { n->right = t; }       //n->right == NULL
-      else if(n->word < t->word) { n->left = t; }   //n->left == NULL
-    }
+     }
+     fix_depth(root,1);
   }
+}
 
   friend ostream &operator<< (ostream &out,const lexicon &l) {
     node *p;
     p = l.root;
+    l.print(out, l.right(p));
+    return out;
   }
 
 
 };
 
-
-void cout (ostream &out, node *p) {
-  while(p->right!=NULL) { p = p->right; }
-  cout << p->word < " " p->freq << endl; 
-  cout (out, p->left);
-  p = p->parent;
-  cout << p->word << " " << p->freq << endl;
-  if(p->left!=NULL) { cout(out, p->left); }
-  
-
+   int main() {
+   lexicon l;
+    l.insert("the");
+    l.insert("boy");
+    l.insert("and");
+    l.insert("the");
+    l.insert("wolf");
+    cout << "The word the is found " << l.lookup("the") << " time(s)" << endl;
+    cout << "The word and is found at depth " << l.depth("and") << endl;
+    cout << l << endl << endl;
+    l.replace("and", "dummy");
+    cout << "After replacement:\n";
+    cout << l ;
+    cout << "Now the word and is found at depth " << l.depth("and") << endl;
+    l.replace("boy", "dummy");
+    cout  << "new lexicon:" << endl << l << endl;
+    //cout << l.depth("dummy");
+    l.replace ("the", "dummy");
+    cout << l << endl;
+    //cout << l.depth("");
+    l.replace ("wolf", "dummy");
+    cout << l;
+    cout << l.lookup("wolf");
 }
-
-
 
 
 
